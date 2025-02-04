a411e2e33c61d90e593295f66ccfbd2b060ec698
DavidOliveira <srnowdkzinx22@gmail.com>
2025-02-04 17:31:45 -0300

    Add new exercicies

--- src/com/loiane/cursojava/exercicio14e15/ex25.java
+++ src/com/loiane/cursojava/exercicio14e15/ex25.java
@@ -1,4 +1,46 @@
 package com.loiane.cursojava.exercicio14e15;
 
+import java.util.Scanner;
+
 public class ex25 {
+    public static void main(String[] args) {
+
+        int[] resposta = new int[5];
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.printf("Digite 1- Sim | 0- Não%n");
+
+        System.out.printf("Telefonou para a vítima? ");
+        resposta[0] = scan.nextInt();
+
+        System.out.printf("Esteve no local do crime? ");
+        resposta[1] = scan.nextInt();
+
+        System.out.printf("Mora perto da vítima? ");
+        resposta[2] = scan.nextInt();
+
+        System.out.printf("Devia para a vítima? ");
+        resposta[3] = scan.nextInt();
+
+        System.out.printf("Já trabalhou com a vítima? ");
+        resposta[4] = scan.nextInt();
+
+        int total = 0;
+        for (int i = 0; i < resposta.length; i++) {
+            total += resposta[i];
+        }
+
+        if (total == 5) {
+            System.out.printf("Assassino!%n");
+        } else if(total > 2 && total < 5) {
+            System.out.printf("Cúmplice!%n");
+        } else if(total > 1 && total < 3) {
+            System.out.printf("Suspeita!%n");
+        } else {
+            System.out.printf("Inocente!%n");
+        }
+
+        scan.close();
+    }
 }
--- src/com/loiane/cursojava/exercicio14e15/ex26.java
+++ src/com/loiane/cursojava/exercicio14e15/ex26.java
@@ -1,4 +1,62 @@
 package com.loiane.cursojava.exercicio14e15;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ex26 {
+    public static void main(String[] args) {
+
+        int litros;
+        String opcao;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.printf("%nQuantos litros você vai por? ");
+        litros = scan.nextInt();
+
+        System.out.printf("Vai abastecer com Gasolina ou Alcool? ");
+        opcao = scan.next();
+
+        calcularCombustivel(opcao, litros);
+
+        scan.close();
+    }
+
+
+    //Metodo calcular combustivel
+    public static void calcularCombustivel(String opcao, int litros) {
+        double desconto = 0;
+        double total = 0;
+
+        switch (opcao.toLowerCase()) {
+            case "Gasolina":
+            case "gasolina":
+                if (litros <= 20) {
+                    desconto = ((litros * 2.50) / 100) * 4;
+                } else {
+                    desconto = ((litros * 2.50) / 100) * 6;
+                }
+                total = (litros * 2.50) - desconto;
+
+                System.out.printf("%d de Gasolina!", litros);
+                System.out.printf("Preço: R$ %.2f", total);
+                System.out.printf("Desconto: R$ %.2f", desconto);
+                break;
+            case "Alcool":
+            case "alcool":
+                if (litros <= 20) {
+                    desconto = ((litros * 1.90) / 100) * 3;
+                } else {
+                    desconto = ((litros * 1.90) / 100) * 5;
+                }
+                total = (litros * 1.90) - desconto;
+                System.out.printf("%d de Alcool!", litros);
+                System.out.printf("Preço: R$ %.2f", total);
+                System.out.printf("Desconto: R$ %.2f", desconto);
+                break;
+            default:
+                System.out.println("Opção inválida. Por favor, escolha entre Gasolina ou Alcool.");
+                break;
+        }
+    }
 }
