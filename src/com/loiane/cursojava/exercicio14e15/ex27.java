3aec4d4c9b5fe139974eb321aba49a3ac9eadba1
DavidOliveira <srnowdkzinx22@gmail.com>
2025-02-06 17:21:17 -0300

    add new exercicies

--- src/com/loiane/cursojava/exercicio14e15/ex27.java
+++ src/com/loiane/cursojava/exercicio14e15/ex27.java
@@ -1,4 +1,59 @@
 package com.loiane.cursojava.exercicio14e15;
 
+import java.sql.SQLOutput;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ex27 {
+    public static void main(String[] args) {
+
+        int pesoMaca;
+        int pesoMorango;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Quantos kilos de Maça?");
+        pesoMaca = scan.nextInt();
+
+        System.out.println("Quantos kilos de Morango?");
+        pesoMorango = scan.nextInt();
+
+        int pesoTotal = pesoMaca + pesoMorango;
+
+        double precoMaca = calcularMaca(pesoMaca);
+        double precoMorango = calcularMorango(pesoMorango);
+
+        double precoTotal = precoMaca + precoMorango;
+
+        verificarDesconto(pesoTotal, precoTotal);
+
+        scan.close();
+    }
+
+    // Metodo calcular preço da MAÇÃ
+    public static double calcularMaca(int pesoMaca) {
+        double precoMaca = (pesoMaca <= 5) ? pesoMaca * 1.80 : pesoMaca * 1.50;
+        System.out.printf("O preço total das Maçãs: R$ %.2f%n", precoMaca);
+        return precoMaca;
+    }
+
+    // Metodo calcular preço do MORANGO
+    public static double calcularMorango(int pesoMorango) {
+        double precoMorango = (pesoMorango <= 5) ? pesoMorango * 2.50 : pesoMorango * 2.20;
+        System.out.printf("O preço total dos Morangos: R$ %.2f%n", precoMorango);
+        return precoMorango;
+    }
+
+    // Metodo que verifica se o peso total > 8 kg ou o preço total > R$ 25,00
+    public static void verificarDesconto(int pesoTotal, double precoTotal) {
+        if (pesoTotal > 8 || precoTotal > 25.00) {
+            double desconto = precoTotal * 0.10; // 10% de desconto
+            double precoFinal = precoTotal - desconto;
+
+            System.out.printf("Desconto aplicado: R$ %.2f%n", desconto);
+            System.out.printf("Preço final com desconto: R$ %.2f%n", precoFinal);
+        } else {
+            System.out.printf("Nenhum desconto aplicado. Preço final: R$ %.2f%n", precoTotal);
+        }
+    }
 }
--- src/com/loiane/cursojava/exercicio14e15/ex28.java
+++ src/com/loiane/cursojava/exercicio14e15/ex28.java
@@ -1,4 +1,92 @@
 package com.loiane.cursojava.exercicio14e15;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ex28 {
+    public static void main(String[] args) {
+
+        int peso = 0;
+        int opcao;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Escolha entre uma dessas opções de carne.");
+        System.out.println("1.File Duplo - 2.Alcatra - 3.Picanha");
+        opcao = scan.nextInt();
+
+        System.out.println("Quantos kilos? ");
+        peso = scan.nextInt();
+
+        System.out.println("Compra no cartão tabajara? 1.Sim 2.Não");
+        int cartao = scan.nextInt();
+
+        double preco = calcularPreco(opcao, peso);
+        exibirInformacoes(opcao, preco, cartao);
+
+        scan.close();
+    }
+
+    // Metodo Escolher opção de carne e retorna o preço
+    public static double calcularPreco(int opcao, int peso) {
+        double preco = 0;
+
+        switch (opcao) {
+            case 1:
+                if (peso <= 5) {
+                    preco = peso * 4.90;
+                } else {
+                    preco = peso * 5.80;
+                }
+                break;
+            case 2:
+                if (peso <= 5) {
+                    preco = peso * 5.90;
+                } else {
+                    preco = peso * 6.80;
+                }
+                break;
+            case 3:
+                if (peso <= 5) {
+                    preco = peso * 6.90;
+                } else {
+                    preco = peso * 7.80;
+                }
+                break;
+            default:
+                System.out.println("Opção Inválida!");
+        }
+        return preco;
+    }
+
+    // Metodo exibir o valor e tipo da carne
+    public static void exibirInformacoes(int opcao, double preco, int cartao) {
+        String tipoCarne;
+        double desconto = 0;
+        double subtotal = 0;
+
+        // exibe o tipo de carne
+        if (opcao == 1) {
+            tipoCarne = "File Duplo";
+        } else if(opcao == 2) {
+            tipoCarne = "Alcatra";
+        } else if(opcao == 3) {
+            tipoCarne = "Picanha";
+        } else {
+            tipoCarne = "Opção Inválida";
+        }
+
+        // opção compra no cartão ou não, para gerar desconto
+        if (cartao == 1) {
+            desconto = (preco / 100) * 5;
+        }
+
+        subtotal = preco - desconto;
+
+        System.out.println("Carne selecionada: " + tipoCarne + "!");
+        System.out.println("Preço: R$ " + preco);
+        System.out.println("Desconto: R$ " + desconto);
+        System.out.println("Subtotal: R$ " + subtotal);
+    }
+
 }
