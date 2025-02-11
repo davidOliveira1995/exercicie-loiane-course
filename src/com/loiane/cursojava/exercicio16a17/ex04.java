9f07386b4ff393604672254a93c66e897a1ad9f2
DavidOliveira <srnowdkzinx22@gmail.com>
2025-02-11 16:51:30 -0300

    Add new exercicie

--- src/com/loiane/cursojava/exercicio16a17/ex04.java
+++ src/com/loiane/cursojava/exercicio16a17/ex04.java
@@ -1,4 +1,24 @@
 package com.loiane.cursojava.exercicio16a17;
 
 public class ex04 {
+    public static void main(String[] args) {
+
+        double populacaoA = 10;
+        double populacaoB = 2000000000;
+
+        calcularCrescimentoPopulacional(populacaoA, populacaoB);
+    }
+
+    // calcular crescimento da população
+    public static void calcularCrescimentoPopulacional(double populacaoA, double populacaoB) {
+        for (int i = 1; populacaoA <= populacaoB; i++) {
+            populacaoA = populacaoA  * 1.03;
+            populacaoB = populacaoB * 1.015;
+
+            System.out.printf("%nAno: %d", i);
+            System.out.printf("%nPopulação A: %.0f", populacaoA);
+            System.out.printf("%nPopulação B: %.0f", populacaoB);
+        }
+
+    }
 }
